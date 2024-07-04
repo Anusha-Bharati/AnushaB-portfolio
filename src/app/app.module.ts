6f52dbfa56d6cd5fcd6667c7c9a2b09da29b3689
anushabharati <anusha.bharati085@gmail.com>
2024-07-05 00:57:39 +0100

    changes for projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { FooterComponent } from './components/general/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { TimelineModule } from 'primeng/timeline';
+import { CardModule } from "primeng/card"; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -21,7 +25,11 @@ import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserAnimationsModule,
     RouterOutlet, 
     RouterModule,
-    NgbNavModule
+    NgbNavModule,
+    TimelineModule,
+    CardModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
